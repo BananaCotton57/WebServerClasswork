f009d6db01e75c6b6c9e2884621bc7e7306874ed
BananaCotton57 <johnnyngo772@gmail.com>
2025-02-27 15:21:10 -0500

    added navigation

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,23 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
   <header>
  
+    <NavBar />
+
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-
   </header>
-
+  <div class="container body-container">
   <RouterView />
+  </div>
 </template>
 
 <style scoped>
+body.container {
+  background-color: aqua;
+}
 </style>
