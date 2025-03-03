198cf47f037c4f331ef636ba21991fea372d75c5
BananaCotton57 <johnnyngo772@gmail.com>
2025-03-03 18:18:30 -0500

    more components

---
+++ client/src/components/ShoppingCart.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1 class="title is-4">Shopping Cart</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/SlidingDrawer.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="sliding-drawer">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+.sliding-drawer {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 70%;
+    max-width: 300px;
+    background-color: white;
+    box-shadow: 2px 0 5px rgba(0, 0, 0, 0.26);
+    z-index: 200;
+    transform: translateX(-100%);
+    transition: transform 0.3s ease-out;
+}
+
+.sliding-drawer.open .sliding-drawer:hover{
+    transform: translateX(0);
+}
+</style>
