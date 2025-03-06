eb06c6b00d342ee7e47294c12aa2be1bf6c6a909
BananaCotton57 <johnnyngo772@gmail.com>
2025-03-06 14:41:58 -0500

    added vue use

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,10 +1,22 @@
+<script setup lang="ts">
+import { useNow } from '@vueuse/core';
+
+// Destructuring useNow to get `now`, `pause`, and `resume`
+const { now, pause, resume } = useNow({ controls: true });
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <h2>BEST CLASS IN NW PALTZ</h2>
+  <div>
+    <h1>This is the About page</h1>
+    <p>Best class in NP</p>
+  </div>
+  <div>
+    <p>Current time: {{ now }}</p>
+    <button @click="pause">Pause</button>
+    <button @click="resume">Resume</button>
   </div>
 </template>
 
-<style>
-
+<style scoped>
 </style>
+
