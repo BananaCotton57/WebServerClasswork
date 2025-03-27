ac882716cd9c1e3962deec2566662f2c6c4b653d
BananaCotton57 <johnnyngo772@gmail.com>
2025-03-27 12:27:07 -0400

    backend

--- server/index.js
+++ server/index.js
@@ -1,17 +1,25 @@
-//load the http module
+/*  B"H
+*/
+// Load the http module to create an http server.
 const express = require('express')
-const PORT = 8000;
+const productsController = require('./controllers/products')
+
+const PORT = 8000
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello New Paltz, NY!!!\n')
-});
+app
+  .get('/', (req, res) => {
+    res.send('Hello New Paltz, NY!!!')
+  })
+  .use('/products', productsController)
 
-
-
+// Listen on port 8000, IP defaults to
+//
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`)
 });
+
+
 
 console.log('Hello World!')
